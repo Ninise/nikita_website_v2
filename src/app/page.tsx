1e59b449d1ae773d6e6d50fccc993f2db871d8ca
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-11-17 22:06:09 -0500

    working on menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import { HomeHeader } from '../components/home/HomeHeader';
 import { HomeLanding } from '../components/home/HomeLanding';
 import { HomeAbout } from '../components/home/HomeAbout';
@@ -7,10 +11,49 @@ import { HomePortfolio } from '../components/home/HomoPortfolio';
 import { HomeFooter } from '../components/home/HomeFooter';
 
 export default function Home() {
+  const showMenu = (flag: boolean) => setShowMenu(flag);
+
+  const [isShowMenu, setShowMenu] = useState(false);
+
   return (
     <div>
       {/* <div className='bg'></div> */}
-      <HomeHeader />
+      <HomeHeader
+        fun={() => {
+          showMenu(!isShowMenu);
+        }}
+      />
+      {isShowMenu ? (
+        <div
+          id='menu'
+          className='fixed z-90 w-0 h-0 flex justify-center items-center bg-gray-900 opacity-0 duration-700'>
+          <a
+            href='javascript:void(0)'
+            className='fixed top-6 right-8 text-white hover:text-amber-500 text-7xl font-semibold duration-300'
+            onClick={() => showMenu(!isShowMenu)}>
+            &times;
+          </a>
+          <div className='flex flex-col text-white text-center text-xl font-light space-y-3'>
+            <a
+              className='hover:text-amber-500 duration-300'
+              href='#'></a>
+            <a
+              className='hover:text-amber-500 duration-300'
+              href='#'></a>
+            <a
+              className='hover:text-amber-500 duration-300'
+              href='#'></a>
+            <a
+              className='hover:text-amber-500 duration-300'
+              href='#'></a>
+            <a
+              className='hover:text-amber-500 duration-300'
+              href='#'></a>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
       <main className='flex min-h-screen flex-col justify-between p-24 max-xl:p-28'>
         <HomeLanding />
         <HomeAbout />
--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,9 +1,11 @@
-export const HomeHeader = () => {
+export const HomeHeader = (fun: () => void) => {
   return (
     <section>
       <nav className='fixed top-0 right-0'>
         <div className=' items-center justify-between mx-auto p-24 max-xl:px-8'>
-          <button className='content-center px-5 py-2 bg-menu-button text-dark-blue hover:text-menu-button hover:bg-dark-blue hover:fill-white fill-dark-blue rounded-lg'>
+          <button
+            className='content-center px-5 py-2 bg-menu-button text-dark-blue hover:text-menu-button hover:bg-dark-blue hover:fill-white fill-dark-blue rounded-lg'
+            onClick={() => fun}>
             <div className='inline-flex items-center space-x-4'>
               <svg
                 width='22'
