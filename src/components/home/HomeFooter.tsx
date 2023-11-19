629dc6025c5291a7d6c53850e9211c0fc938e8d0
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-11-19 07:56:48 -0500

    added responsiv to menu

--- src/components/home/HomeFooter.tsx
+++ src/components/home/HomeFooter.tsx
@@ -2,7 +2,7 @@ export const HomeFooter = ({ isFooter }: { isFooter: boolean }) => {
   const TYPE_FOOTER: string =
     'flex max-xl:flex-col flex-row justify-between justify-self-center space-x-36 max-xl:space-x-0';
   const TYPE_MENU: string =
-    'flex flex-row justify-between justify-self-center space-x-36 max-xl:space-x-0';
+    'flex flex-row justify-between justify-self-center space-x-36 max-xl:space-x-0 max-sm:flex-col';
 
   return (
     <section className='grid pt-72'>
@@ -18,56 +18,64 @@ export const HomeFooter = ({ isFooter }: { isFooter: boolean }) => {
             </h2>
             <div
               className={
-                'flex flex-col gap-y-8 ' +
-                (isFooter ? 'text-base' : 'text-xl max-2xl:text-base')
+                '' +
+                (isFooter
+                  ? ''
+                  : 'flex flex-row justify-between w-full px-12 max-sm:px-0')
               }>
-              <a
-                href='https://www.linkedin.com/in/ninise/'
-                className='text-dark-white underline underline-offset-4'>
-                Linkedin
-              </a>
+              <div
+                className={
+                  'flex flex-col gap-y-8 ' +
+                  (isFooter ? 'text-base' : 'text-xl max-2xl:text-base')
+                }>
+                <a
+                  href='https://www.linkedin.com/in/ninise/'
+                  className='text-dark-white underline underline-offset-4'>
+                  Linkedin
+                </a>
 
-              <a
-                href='https://github.com/ninise'
-                className='text-dark-white underline underline-offset-4'>
-                GitHub
-              </a>
-            </div>
+                <a
+                  href='https://github.com/ninise'
+                  className='text-dark-white underline underline-offset-4'>
+                  GitHub
+                </a>
+              </div>
 
-            <div
-              className={
-                'flex flex-col gap-y-8 ' +
-                (isFooter ? 'text-base' : 'text-xl max-2xl:text-base')
-              }>
-              <a
-                href='linked'
-                className='text-dark-white underline underline-offset-4'>
-                Whatsapp
-              </a>
+              <div
+                className={
+                  'flex flex-col gap-y-8 ' +
+                  (isFooter ? 'text-base' : 'text-xl max-2xl:text-base')
+                }>
+                <a
+                  href='linked'
+                  className='text-dark-white underline underline-offset-4'>
+                  Whatsapp
+                </a>
 
-              <a
-                href='linked'
-                className='text-dark-white underline underline-offset-4'>
-                Email
-              </a>
-            </div>
+                <a
+                  href='linked'
+                  className='text-dark-white underline underline-offset-4'>
+                  Email
+                </a>
+              </div>
 
-            <div
-              className={
-                'flex flex-col gap-y-8 ' +
-                (isFooter ? 'text-base' : 'text-xl max-2xl:text-base')
-              }>
-              <a
-                href='https://www.upwork.com/freelancers/~01890cef721cd87654'
-                className='text-dark-white underline underline-offset-4'>
-                Upwork
-              </a>
+              <div
+                className={
+                  'flex flex-col gap-y-8 ' +
+                  (isFooter ? 'text-base' : 'text-xl max-2xl:text-base')
+                }>
+                <a
+                  href='https://www.upwork.com/freelancers/~01890cef721cd87654'
+                  className='text-dark-white underline underline-offset-4'>
+                  Upwork
+                </a>
 
-              <a
-                href='linked'
-                className='text-dark-white underline underline-offset-4'>
-                Resume
-              </a>
+                <a
+                  href='linked'
+                  className='text-dark-white underline underline-offset-4'>
+                  Resume
+                </a>
+              </div>
             </div>
           </div>
 
--- src/components/home/HomeMenu.tsx
+++ src/components/home/HomeMenu.tsx
@@ -8,6 +8,7 @@ type HomeHeaderProps<T> = {
 
 export const HomeMenu: React.FC<HomeHeaderProps<() => void>> = ({ fun }) => {
   const TABLET_BTN: string = 'max-xl:px-3 max-xl:py-3 max-xl:right-12';
+  const MOBILE_BTN: string = ' max-sm:px-1 max-sm:w-12';
 
   return (
     <div
@@ -17,16 +18,17 @@ export const HomeMenu: React.FC<HomeHeaderProps<() => void>> = ({ fun }) => {
         <button
           className={
             'flex flew-row fixed top-24 right-24 items-center px-5 py-2 bg-menu-back-button text-dark-blue hover:text-menu-button hover:bg-dark-blue fill-dark-blue rounded-lg ' +
-            TABLET_BTN
+            TABLET_BTN +
+            MOBILE_BTN
           }
           onClick={() => fun()}>
-          <div className='inline-flex items-center space-x-4 max-xl:space-x-2'>
+          <div className='flex flex-row items-center space-x-4 max-xl:space-x-2 max-sm:space-x-0'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               width='28'
               height='28'
               viewBox='0 0 28 28'
-              className='max-xl:h-6'
+              className='max-xl:h-6 max-sm:ms-1.5'
               fill='none'>
               <path
                 d='M13.9999 15.6333L8.28327 21.35C8.06938 21.5639 7.79716 21.6708 7.4666 21.6708C7.13605 21.6708 6.86382 21.5639 6.64993 21.35C6.43605 21.1361 6.3291 20.8639 6.3291 20.5333C6.3291 20.2028 6.43605 19.9306 6.64993 19.7167L12.3666 14L6.64993 8.28333C6.43605 8.06944 6.3291 7.79722 6.3291 7.46666C6.3291 7.13611 6.43605 6.86388 6.64993 6.65C6.86382 6.43611 7.13605 6.32916 7.4666 6.32916C7.79716 6.32916 8.06938 6.43611 8.28327 6.65L13.9999 12.3667L19.7166 6.65C19.9305 6.43611 20.2027 6.32916 20.5333 6.32916C20.8638 6.32916 21.136 6.43611 21.3499 6.65C21.5638 6.86388 21.6708 7.13611 21.6708 7.46666C21.6708 7.79722 21.5638 8.06944 21.3499 8.28333L15.6333 14L21.3499 19.7167C21.5638 19.9306 21.6708 20.2028 21.6708 20.5333C21.6708 20.8639 21.5638 21.1361 21.3499 21.35C21.136 21.5639 20.8638 21.6708 20.5333 21.6708C20.2027 21.6708 19.9305 21.5639 19.7166 21.35L13.9999 15.6333Z'
@@ -34,12 +36,12 @@ export const HomeMenu: React.FC<HomeHeaderProps<() => void>> = ({ fun }) => {
               />
             </svg>
 
-            <p className='text-lg font-medium text-dark-white tracking-widest max-xl:text-base'>
+            <p className='text-lg font-medium text-dark-white max-xl:text-base max-sm:collapse'>
               CLOSE
             </p>
           </div>
         </button>
-        <div className='flex flex-row w-10/12 text-dark-white text-2xl text-medium self-center justify-between mt-18 align-center max-xl:text-xl'>
+        <div className='flex flex-row w-10/12 text-dark-white text-2xl text-medium self-center justify-between mt-18 align-center max-xl:text-xl max-sm:text-sm'>
           <a
             className='hover:text-white duration-300'
             href='#'>
@@ -89,9 +91,9 @@ export const HomeMenu: React.FC<HomeHeaderProps<() => void>> = ({ fun }) => {
                 width='795'
                 height='795'
                 filterUnits='userSpaceOnUse'
-                color-interpolation-filters='sRGB'>
+                colorInterpolationFilters='sRGB'>
                 <feFlood
-                  flood-opacity='0'
+                  floodOpacity='0'
                   result='BackgroundImageFix'
                 />
                 <feBlend
@@ -135,9 +137,9 @@ export const HomeMenu: React.FC<HomeHeaderProps<() => void>> = ({ fun }) => {
                 width='795'
                 height='795'
                 filterUnits='userSpaceOnUse'
-                color-interpolation-filters='sRGB'>
+                colorInterpolationFilters='sRGB'>
                 <feFlood
-                  flood-opacity='0'
+                  floodOpacity='0'
                   result='BackgroundImageFix'
                 />
                 <feBlend
@@ -181,9 +183,9 @@ export const HomeMenu: React.FC<HomeHeaderProps<() => void>> = ({ fun }) => {
                 width='795'
                 height='795'
                 filterUnits='userSpaceOnUse'
-                color-interpolation-filters='sRGB'>
+                colorInterpolationFilters='sRGB'>
                 <feFlood
-                  flood-opacity='0'
+                  floodOpacity='0'
                   result='BackgroundImageFix'
                 />
                 <feBlend
