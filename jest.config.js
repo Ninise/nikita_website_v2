6a4b32cee3ea18a36292a5195031e38fd25a8f86
Ninise <nikitin.ninise@gmail.com>
2025-03-07 14:20:34 -0500

    fix tests configuration and remove custom bable

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
-  },
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
